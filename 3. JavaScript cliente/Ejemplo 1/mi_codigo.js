569e7be85768ea3058b780730c91260866de91a9
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-02-28 18:59:32 +0000

    Add code to demo adding HTML dynamically

--- 3. JavaScript cliente/Ejemplo 1/mi_codigo.js
+++ 3. JavaScript cliente/Ejemplo 1/mi_codigo.js
@@ -1,6 +1,14 @@
-const mi_boton = document.getElementById("mi_buton");
+const mi_boton = document.getElementById('mi_buton');
+const mi_lista = document.getElementById('mi_lista');
 mi_boton.addEventListener('click', presionar_boton);
 
+let contador = 0;
+
+const colores = ['red', 'green', 'blue'];
+
 function presionar_boton() {
-  alert('Presionaste el botón');
+  contador++;
+  mi_lista.innerHTML += '<li>Una opción ' + contador + '</li>';
+  // mi_lista.style.color = colores[contador % colores.length];
+  mi_lista.style['color'] = colores[contador % colores.length];
 }
