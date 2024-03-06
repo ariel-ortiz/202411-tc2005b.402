c153d4e44f8bd3961a68c987d4ba0b8a5c549a15
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-03-06 18:58:51 +0000

    Create hola.js file to demo using node

---
+++ 4. JavaScript servidor/Ejemplo 1/hola.js
@@ -0,0 +1,13 @@
+/*
+ * Este es un comentario de bloque.
+ */
+
+main();
+
+// Este es un comentario de línea.
+
+function main() {
+  for (let i = 0; i < 5; i++) {
+    console.log(`${i} ¡Hola, mundo!`);
+  }
+}
